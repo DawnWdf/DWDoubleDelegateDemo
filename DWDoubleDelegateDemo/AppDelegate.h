e880d3a81ff7176b0ce3b35a275293c37cd72658
Dawn <icy19882006@163.com>
2017-07-03 16:36:21 +0800

    Initial Commit

---
+++ DWDoubleDelegateDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DWDoubleDelegateDemo
+//
+//  Created by Dawn Wang on 2017/7/3.
+//  Copyright © 2017年 Dawn Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DWDoubleDelegateDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DWDoubleDelegateDemo
+//
+//  Created by Dawn Wang on 2017/7/3.
+//  Copyright © 2017年 Dawn Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
